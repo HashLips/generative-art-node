50b677223d3f632b3d19c4e757bd8126ddd80e58
Kalin Ramsey <kalinramsey@gmail.com>
2021-12-10 22:26:13 -0700

    Adjusted logic to enable dynamic file structure/sizing. Can now define numberous styles for a given attribute, with each having its own unique variety of further details.

--- src/config.js
+++ src/config.js
@@ -1,23 +1,39 @@
+// const layersOrder = [
+//     { name: 'background', number: 1 },
+//     { name: 'ball', number: 2 },
+//     { name: 'eye color', number: 12 },
+//     { name: 'iris', number: 3 },
+//     { name: 'shine', number: 1 },
+//     { name: 'shine', number: 1 },
+//     { name: 'bottom lid', number: 3 },
+//     { name: 'top lid', number: 3 },
+// ];
+
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+	{ name: "Background" },
+	{ name: "Abdomen" },
+	{ name: "Wings" },
+	{ name: "LegLeft" },
+	{ name: "Thorax" },
+	{ name: "LegRight" },
+	{ name: "EyeLeft" },
+	{ name: "Mouth" },
+	{ name: "EyeRight" },
+	{ name: "Horn" },
 ];
-  
+
 const format = {
-    width: 230,
-    height: 230
+	width: 256,
+	height: 256,
 };
 
 const rarity = [
-    { key: "", val: "original" },
-    { key: "_r", val: "rare" },
-    { key: "_sr", val: "super rare" },
+	{ key: "", val: "common" },
+	{ key: "_uc", val: "uncommon" },
+	{ key: "_r", val: "rare" },
+	{ key: "_ep", val: "epic" },
+	{ key: "_lg", val: "legendary" },
+	{ key: "_uq", val: "unique" },
 ];
 
 const defaultEdition = 5;
--- src/main.js
+++ src/main.js
@@ -7,11 +7,11 @@ const canvas = createCanvas(format.width, format.height);
 const ctx = canvas.getContext("2d");
 
 if (!process.env.PWD) {
-  process.env.PWD = process.cwd();
+	process.env.PWD = process.cwd();
 }
 
 const buildDir = `${process.env.PWD}/build`;
-const metDataFile = '_metadata.json';
+const metDataFile = "_metadata.json";
 const layersDir = `${process.env.PWD}/layers`;
 
 let metadata = [];
@@ -20,148 +20,248 @@ let hash = [];
 let decodedHash = [];
 const Exists = new Map();
 
+const addRarity = (_str) => {
+	let itemRarity;
 
-const addRarity = _str => {
-  let itemRarity;
+	rarity.forEach((r) => {
+		if (_str.includes(r.key)) {
+			itemRarity = r.val;
+		}
+	});
 
-  rarity.forEach((r) => {
-    if (_str.includes(r.key)) {
-      itemRarity = r.val;
-    }
-  });
-
-  return itemRarity;
+	return itemRarity;
 };
 
-const cleanName = _str => {
-  let name = _str.slice(0, -4);
-  rarity.forEach((r) => {
-    name = name.replace(r.key, "");
-  });
-  return name;
+const cleanName = (_str) => {
+	let name = _str.slice(0, -4);
+	rarity.forEach((r) => {
+		name = name.replace(r.key, "");
+	});
+	return name;
 };
 
-const getElements = path => {
-  return fs
-    .readdirSync(path)
-    .filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
-    .map((i, index) => {
-      return {
-        id: index + 1,
-        name: cleanName(i),
-        fileName: i,
-        rarity: addRarity(i),
-      };
-    });
+const getElements = (path) => {
+	return fs
+		.readdirSync(path)
+		.filter((item) => !/(^|\/)\.[^\/\.]/g.test(item))
+		.map((i, index) => {
+			return {
+				id: index + 1,
+				name: cleanName(i),
+				fileName: i,
+				rarity: addRarity(i),
+			};
+		});
 };
 
-const layersSetup = layersOrder => {
-  const layers = layersOrder.map((layerObj, index) => ({
-    id: index,
-    name: layerObj.name,
-    location: `${layersDir}/${layerObj.name}/`,
-    elements: getElements(`${layersDir}/${layerObj.name}/`),
-    position: { x: 0, y: 0 },
-    size: { width: format.width, height: format.height },
-    number: layerObj.number
-  }));
+const layersSetup = (layersOrder) => {
+	/**
+	 * --- DYNAMIC FOLDER STRUCTURE
+	 * Now you can add multiple 'Style[i]' folders and various 'Pattern[j]'
+	 * This lets you add patterns/details on top of a predetermined form(style)
+	 * Example: A cat in Pose A wears a cape, while a spider in Pose B wears boots on each leg.
+	 * 8 boots do not map well to a cat, but now you can have multiple spiders that will map to the boot accordingly
+	 * AND a variety of cats that map well to the same assets
+	 */
+	const layers = [];
+	let extraIndex = 0;
 
-  return layers;
+	layersOrder.map((layerObj, index) => {
+		// primary asset folder and asset count
+		let path = `${layersDir}/${layerObj.name}/`;
+		let count = getElements(path).length;
+
+		// determine if asset has different styles
+		const hasDiffStyles = getElements(path).find((a) =>
+			a.fileName.includes("Style")
+		);
+
+		// if it has different styles:
+		if (hasDiffStyles) {
+			// find how many different styles
+			// pick a random style to build upon
+			// operate within the style's directory
+			const maxStyles = count;
+			let style = Math.floor(Math.random() * maxStyles) + 1;
+			path += `Style${style}/`;
+			count = getElements(path).length;
+
+			// check to see if a given style has various patterns
+			const hasDiffPatterns = getElements(path).find((s) =>
+				s.fileName.includes("Patterns")
+			);
+			// if it has various patterns, return style, then pattern.
+			// else just return style
+			if (hasDiffPatterns) {
+				// if it has different patterns:
+
+				// find how many different patterns
+				// pick a random pattern
+				// operate within the pattern's directory
+				const maxPatterns = getElements(`${path}Patterns/`).length;
+				const pattern = Math.floor(Math.random() * maxPatterns) + 1;
+
+				// to find how many styles are available, subtract the number of Patterns in the accompanying folder
+				const numPatterns = getElements(`${path}`).filter((f) =>
+					f.fileName.includes("Pattern")
+				).length;
+				count -= numPatterns;
+
+				// return pattern and style
+				layers.push({
+					id: index + extraIndex,
+					name: `${layerObj.name}_${style}`,
+					location: path,
+					elements: getElements(path),
+					position: { x: 0, y: 0 },
+					size: { width: format.width, height: format.height },
+					number: count,
+				});
+
+				extraIndex++;
+				path += `Patterns/Pattern${pattern}/`;
+				count = getElements(path).length;
+
+				layers.push({
+					id: index + extraIndex,
+					name: `${layerObj.name}_${style}_${pattern}`,
+					location: path,
+					elements: getElements(path),
+					position: { x: 0, y: 0 },
+					size: { width: format.width, height: format.height },
+					number: count,
+				});
+			} else {
+				// if it does not have different patterns:
+
+				// return the style in the current folder
+				layers.push({
+					id: index + extraIndex,
+					name: `${layerObj.name}_${style}`,
+					location: path,
+					elements: getElements(path),
+					position: { x: 0, y: 0 },
+					size: { width: format.width, height: format.height },
+					number: count,
+				});
+			}
+		} else {
+			// if it doesnt have different styles:
+
+			// return elements in current folder
+			layers.push({
+				id: index + extraIndex,
+				name: layerObj.name,
+				location: path,
+				elements: getElements(path),
+				position: { x: 0, y: 0 },
+				size: { width: format.width, height: format.height },
+				number: count,
+			});
+		}
+	});
+	return layers;
 };
 
 const buildSetup = () => {
-  if (fs.existsSync(buildDir)) {
-    fs.rmdirSync(buildDir, { recursive: true });
-  }
-  fs.mkdirSync(buildDir);
+	if (fs.existsSync(buildDir)) {
+		fs.rmdirSync(buildDir, { recursive: true });
+	}
+	fs.mkdirSync(buildDir);
 };
 
 const saveLayer = (_canvas, _edition) => {
-  fs.writeFileSync(`${buildDir}/${_edition}.png`, _canvas.toBuffer("image/png"));
+	fs.writeFileSync(
+		`${buildDir}/${_edition}.png`,
+		_canvas.toBuffer("image/png")
+	);
 };
 
-const addMetadata = _edition => {
-  let dateTime = Date.now();
-  let tempMetadata = {
-    hash: hash.join(""),
-    decodedHash: decodedHash,
-    edition: _edition,
-    date: dateTime,
-    attributes: attributes,
-  };
-  metadata.push(tempMetadata);
-  attributes = [];
-  hash = [];
-  decodedHash = [];
+const addMetadata = (_edition) => {
+	let dateTime = Date.now();
+	let tempMetadata = {
+		hash: hash.join(""),
+		decodedHash: decodedHash,
+		edition: _edition,
+		date: dateTime,
+		attributes: attributes,
+	};
+	metadata.push(tempMetadata);
+	attributes = [];
+	hash = [];
+	decodedHash = [];
 };
 
 const addAttributes = (_element, _layer) => {
-  let tempAttr = {
-    id: _element.id,
-    layer: _layer.name,
-    name: _element.name,
-    rarity: _element.rarity,
-  };
-  attributes.push(tempAttr);
-  hash.push(_layer.id);
-  hash.push(_element.id);
-  decodedHash.push({ [_layer.id]: _element.id });
+	let tempAttr = {
+		id: _element.id,
+		layer: _layer.name,
+		name: _element.name,
+		rarity: _element.rarity,
+	};
+	attributes.push(tempAttr);
+	hash.push(_layer.id);
+	hash.push(_element.id);
+	decodedHash.push({ [_layer.id]: _element.id });
 };
 
 const drawLayer = async (_layer, _edition) => {
-  const rand = Math.random();
-  let element =
-    _layer.elements[Math.floor(rand * _layer.number)] ? _layer.elements[Math.floor(rand * _layer.number)] : null;
-  if (element) {
-    addAttributes(element, _layer);
-    const image = await loadImage(`${_layer.location}${element.fileName}`);
+	const rand = Math.random();
+	let element = _layer.elements[Math.floor(rand * _layer.number)]
+		? _layer.elements[Math.floor(rand * _layer.number)]
+		: null;
+	if (element) {
+		addAttributes(element, _layer);
+		const image = await loadImage(`${_layer.location}${element.fileName}`);
 
-    ctx.drawImage(
-      image,
-      _layer.position.x,
-      _layer.position.y,
-      _layer.size.width,
-      _layer.size.height
-    );
-    saveLayer(canvas, _edition);
-  }
+		ctx.drawImage(
+			image,
+			_layer.position.x,
+			_layer.position.y,
+			_layer.size.width,
+			_layer.size.height
+		);
+		saveLayer(canvas, _edition);
+	}
 };
 
-const createFiles = async edition => {
-  const layers = layersSetup(layersOrder);
+const createFiles = async (edition) => {
+	let numDupes = 0;
+	for (let i = 1; i <= edition; i++) {
+		const layers = layersSetup(layersOrder);
+		await layers.forEach(async (layer) => {
+			await drawLayer(layer, i);
+		});
 
-  let numDupes = 0;
- for (let i = 1; i <= edition; i++) {
-   await layers.forEach(async (layer) => {
-     await drawLayer(layer, i);
-   });
-
-   let key = hash.toString();
-   if (Exists.has(key)) {
-     console.log(
-       `Duplicate creation for edition ${i}. Same as edition ${Exists.get(
-         key
-       )}`
-     );
-     numDupes++;
-     if (numDupes > edition) break; //prevents infinite loop if no more unique items can be created
-     i--;
-   } else {
-     Exists.set(key, i);
-     addMetadata(i);
-     console.log("Creating edition " + i);
-   }
- }
+		let key = hash.toString();
+		if (Exists.has(key)) {
+			console.log(
+				`Duplicate creation for edition ${i}. Same as edition ${Exists.get(
+					key
+				)}`
+			);
+			numDupes++;
+			if (numDupes > edition) break; //prevents infinite loop if no more unique items can be created
+			i--;
+		} else {
+			Exists.set(key, i);
+			addMetadata(i);
+			console.log("Creating edition " + i);
+		}
+	}
 };
 
 const createMetaData = () => {
-  fs.stat(`${buildDir}/${metDataFile}`, (err) => {
-    if(err == null || err.code === 'ENOENT') {
-      fs.writeFileSync(`${buildDir}/${metDataFile}`, JSON.stringify(metadata, null, 2));
-    } else {
-        console.log('Oh no, error: ', err.code);
-    }
-  });
+	fs.stat(`${buildDir}/${metDataFile}`, (err) => {
+		if (err == null || err.code === "ENOENT") {
+			fs.writeFileSync(
+				`${buildDir}/${metDataFile}`,
+				JSON.stringify(metadata, null, 2)
+			);
+		} else {
+			console.log("Oh no, error: ", err.code);
+		}
+	});
 };
 
 module.exports = { buildSetup, createFiles, createMetaData };
