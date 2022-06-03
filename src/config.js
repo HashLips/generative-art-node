199ae9c55a107eb4ead154c45730184c52c6c350
ItsJackAnton <jacksa999@hotmail.com>
2022-06-03 05:21:54 -0700

    init

--- src/config.js
+++ src/config.js
@@ -1,23 +1,20 @@
 const layersOrder = [
-    { name: 'background', number: 1 },
-    { name: 'ball', number: 2 },
-    { name: 'eye color', number: 12 },
-    { name: 'iris', number: 3 },
-    { name: 'shine', number: 1 },
-    { name: 'shine', number: 1 },
-    { name: 'bottom lid', number: 3 },
-    { name: 'top lid', number: 3 },
+  { name: "background", number: 7 },
+  { name: "body", number: 7 },
+  { name: "eyes", number: 6 },
+  { name: "mouth", number: 7 },
+  { name: "tattoo", number: 7 },
 ];
-  
+
 const format = {
-    width: 230,
-    height: 230
+  width: 230,
+  height: 230,
 };
 
 const rarity = [
-    { key: "", val: "original" },
-    { key: "_r", val: "rare" },
-    { key: "_sr", val: "super rare" },
+  { key: "", val: "original" },
+  { key: "_r", val: "rare" },
+  { key: "_sr", val: "super rare" },
 ];
 
 const defaultEdition = 5;
